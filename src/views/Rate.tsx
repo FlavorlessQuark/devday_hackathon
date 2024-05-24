79b1d429accd78f968624c418f32224c4715ee24
FlavorlessQuark <flavorlessquark@gmail.com>
2024-05-24 01:13:23 -0700

    frontend for real this time

--- src/views/Rate.tsx
+++ src/views/Rate.tsx
@@ -14,24 +14,48 @@ const Rate = () => {
         ];
 
     const [selected, setSelected] = useState(-1);
+    const [value, setValue] = useState(0);
     const {vselect} = useFlags();
 
     return (
         <Container>
             {selected >= 0 &&
             <SelectedContainer>
-                <SelectedImage src={dummydata[selected].img}/>
-                <Row>
+                <Popup>
+                    <Cross onClick={() => setSelected(-1)}> X </Cross>
+                    <SelectedImage src={dummydata[selected].img}/>
+                    <SliderContainer>
+                        <SelectedSlider onChange={(e) => setValue(e.target.valueAsNumber)} type="range" orient="vertical" stpe="1" min="0" max="10" list="Svalues"/>
+                        <datalist id="Svalues">
+                            <option value="0" label="Lame"></option>
+                            <option value="1"></option>
+                            <option value="2"></option>
+                            <option value="3" ></option>
+                            <option value="4" ></option>
+                            <option value="5" label="Neutral"></option>
+                            <option value="6"></option>
+                            <option value="7"></option>
+                            <option value="8" ></option>
+                            <option value="9" ></option>
+                            <option value="10" label="Awesome!"></option>
+                        </datalist>
+                        <SliderValue> {value} / 10</SliderValue>
 
-                <Button onClick={() => setSelected(-1)}> Meh</Button> <Button onClick={() =>setSelected(-1)}> Okay</Button> <Button onClick={() =>setSelected(-1)}> Best router ever!</Button>
-                </Row>
+                        <SliderSubmit> Rate it </SliderSubmit>
+                    </SliderContainer>
+                        {/* <Row>
+                            <Button onClick={() => setSelected(-1)}> Meh</Button>
+                            <Button onClick={() =>setSelected(-1)}> Okay</Button>
+                            <Button onClick={() =>setSelected(-1)}> Best router ever!</Button>
+                        </Row> */}
+                </Popup>
             </SelectedContainer> }
             <Row>
                 {
                     Object.keys(dummydata).map((e,i) => (
                     <RouteContainer>
                         <ImgTile>
-                            <Im onClick={() => setSelected(i)} src={dummydata[e].img}/>
+                            <Im onClick={() => {setSelected(i); setValue(0)}} src={dummydata[e].img}/>
                         </ImgTile>
                         <FlagContainer>
                             {
@@ -54,6 +78,45 @@ const Rate = () => {
         justify-content: center;
     `
 
+    const Cross = styled.div`
+        display: flex;
+        border: 1px solid black;
+        height: fit-content;
+        font-size: 80px;
+        padding: 10px 20px;
+        color: red;
+        &:hover {
+            cursor:pointer;
+        }
+    `
+
+    const SliderContainer = styled.div`
+        display: flex;
+        width: 100%;
+        height: 100%;
+        flex-direction: column;
+        gap: 20px;
+        align-items: center;
+        justify-content: center;
+    `
+
+    const SliderValue = styled.div`
+        font-size: 20px;
+    `
+
+    const SliderSubmit = styled.div`
+    display: flex;
+    border: 1px solid black;
+    padding: 10px 10px;
+    font-size: 30px;
+    background: #1aff3e5e;
+
+    &:hover {
+        cursor:pointer;
+        background: #22e341e3;
+    }
+    `
+
     const RouteContainer  = styled.div`
         display: flex;
         flex-direction: column;
@@ -61,7 +124,16 @@ const Rate = () => {
         border: 2px solid grey;
         padding: 20px 20px;
         border-radius: 4px;
+        align-items: center;
     `
+
+    const Popup = styled.div`
+        display: flex;
+        flex-direction: row;
+        height: 60%;
+        width: 50%;
+    `
+
     const ImgTile = styled.div`
         height: 80%;
         border: 1px solid black;
@@ -84,17 +156,46 @@ const Rate = () => {
         display: flex;
         position: absolute;
         background: #0f0b0b91;
-        width: 100vw;
+        width: 100%;
         height:100%;
+        top: 0;
+        left: 0;
         flex-direction: column;
         align-items: center;
+        justify-content: center;
     `
     const SelectedImage = styled.img`
-    width: 400px;
-        height: 400px;
+    // width: 400px;
+    //     height: 400px;
+    border: 1px solid black;
     `
-    const SelectedSlider = styled.div`
+    const SelectedSlider = styled.input`
+         writing-mode: vertical-lr;
+        direction: rtl;
+        appearance: slider-vertical;
+        width: 30px;
+        height: 90%;
+        vertical-align: bottom;
+        &::-moz-range-track  {
+             width: 100%;
+            height: 100%;
+            cursor: pointer;
+            animate: 0.2s;
+            background: rgba(131, 238, 255, 0.57);
+            border-radius: 16px;
+            border: 1px solid #000000;
+        }
 
+        &::-moz-range-thumb {
+            border: 1px solid #000000;
+            // height: 30px;
+            // width: 25px;
+            width: 110%;
+            height: 10%;
+            border-radius: 40px;
+            background: #FFFFFF;
+            cursor: pointer;
+        }
     `
     const SelectedSubmit = styled.div`
     `
